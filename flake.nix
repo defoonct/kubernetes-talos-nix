b19a08889052c16c5efa3dab7380d6aa3e71c6f3
Diego Escalante <105992129+defoonct@users.noreply.github.com>
2024-05-22 22:30:02 +0200

    Syntax simplification with 'with' expression

--- flake.nix
+++ flake.nix
@@ -4,15 +4,12 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
   outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  let pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       name = "Kubernetes Lab Environment";
 
-      buildInputs = [
-        pkgs.curl
-      ];
+      buildInputs = with pkgs; [ curl ];
 
       shellHook = "echo '=> Entering \"Kubernetes Lab\" environment.'";
     };
